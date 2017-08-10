28bcbba95c6c3d344549175010701b70ca9fe0ad
Danny Dudzic <danieldudzic@gmail.com>
2017-08-10 23:17:15 +0200

    Remove duplicate code in customizer.js

--- assets/js/customizer.js
+++ assets/js/customizer.js
@@ -39,24 +39,4 @@
 			}
 		} );
 	} );
-
-	// Header text color.
-	wp.customize( 'header_textcolor', function( value ) {
-		value.bind( function( to ) {
-			if ( 'blank' === to ) {
-				$( '.site-title, .site-description' ).css( {
-					'clip': 'rect(1px, 1px, 1px, 1px)',
-					'position': 'absolute'
-				} );
-			} else {
-				$( '.site-title, .site-description' ).css( {
-					'clip': 'auto',
-					'position': 'relative'
-				} );
-				$( '.site-description' ).css( {
-					'color': to
-				} );
-			}
-		} );
-	} );
 } )( jQuery );
