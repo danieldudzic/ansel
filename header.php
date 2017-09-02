ec6828aec674194298a590cd5863e8a475afdba9
Danny Dudzic <danieldudzic@gmail.com>
2017-09-02 20:04:43 +0200

    Add navigation styling fixes

--- header.php
+++ header.php
@@ -37,6 +37,13 @@
 				else : ?>
 					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
 					<?php
+				endif;
+
+				$description = get_bloginfo( 'description', 'display' );
+
+				if ( $description || is_customize_preview() ) : ?>
+					<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
+					<?php
 				endif; ?>
 			</div><!-- .site-branding -->
 
@@ -61,14 +68,6 @@
 				?>
 			</nav><!-- #site-navigation -->
 		</div><!-- .branding-container -->
-
-		<?php
-		$description = get_bloginfo( 'description', 'display' );
-
-		if ( $description || is_customize_preview() ) : ?>
-			<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-		endif; ?>
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
