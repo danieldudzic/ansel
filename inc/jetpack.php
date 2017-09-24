1a12760d0b9ac97cef2f4c0c08d83240b2927d88
Danny Dudzic <danieldudzic@gmail.com>
2017-09-25 00:53:40 +0200

    Add portfolio description fixes

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -146,8 +146,8 @@ function ansel_portfolio_content( $before = '', $after = '' ) {
 	$jetpack_portfolio_content = get_option( 'jetpack_portfolio_content' );
 
 	if ( is_tax() && get_the_archive_description() ) {
-		echo $before . get_the_archive_description() . $after;
-	} else if ( isset( $jetpack_portfolio_content ) && '' != $jetpack_portfolio_content ) {
+		return $before . get_the_archive_description() . $after;
+	} elseif ( isset( $jetpack_portfolio_content ) && '' != $jetpack_portfolio_content ) {
 		$content = convert_chars( convert_smilies( wptexturize( stripslashes( wp_filter_post_kses( addslashes( $jetpack_portfolio_content ) ) ) ) ) );
 		return $before . $content . $after;
 	}
